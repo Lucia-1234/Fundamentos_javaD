cbe936ed53dbfd7b1d84bc33832b5f265a2f1c1b
Lucia <Lulizcano.aa@hotmail.com>
2025-11-18 17:57:05 -0500

    se finalizo el ejercicio4

---
+++ Ejercicio4/app.js
@@ -0,0 +1,12 @@
+// 4 Consumo de gasolina
+
+// se solicitan los kilometros  galones sonsumidos 
+let kilometros = parseFloat(prompt("ingrese la cantidad de kilometros recorridos: "));
+let consumo = parseFloat(prompt("ingrese la cantidad de consumo por kilometro: "));
+
+// se realiza el calculo
+let calculo = kilometros * consumo;
+
+// se muestran los resultados 
+console.log("El total de litros consumidos es de: " + total);
+alert ("El total de litros consumidos es de: " + total);
