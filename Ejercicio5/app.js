a1e308e8dbaf95d2fcb6e41e133007bc774a18ee
Lucia <Lulizcano.aa@hotmail.com>
2025-11-18 17:57:20 -0500

    se finalizo el ejercicio5

---
+++ Ejercicio5/app.js
@@ -0,0 +1,8 @@
+// 5 tiempo de viaje
+
+let kilometros = parseFloat(prompt("Ingrese la distancia del recorrido: "))
+let velocidad = parseFloat(prompt("ingrese la veelocidad promedio del recorrido"));
+
+let calculo = kilometros / velocidad;
+
+console.log("El tiempo estimado del viaje en horas es de: " + calculo); 
