46dfd83c56a349e9c715ba71e7ba6fee5ee4c9d9
Lucia-1234 <Lulizcano.aa@hotmail.com>
2025-11-18 19:46:54 -0500

    Se finalizo el ejercicio6

---
+++ Ejercicio6/app.js
@@ -0,0 +1,11 @@
+// 6 Conversion de minutos a horas y minutos 
+
+//se solicitan los minutos 
+let minutos = parseInt(prompt("Ingrese el tiempo en minutos: "));
+
+// constante que hace el calculo de minutos a horas 
+const horas= minutos/60;
+
+//se muestran los resultados 
+console.log(+horas+ " horas" +minutos+ " minutos");
+alert (+horas+ " horas " +minutos+ " minutos");
